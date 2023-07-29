599dee2174875884519a4eaa33bf69fd1f40e20a
Den104Ten <dan_eltsov@mail.ru>
2023-07-29 15:57:31 +0300

    Новая страничка register_incorrect скопируй ее

--- WEB_FL.py
+++ WEB_FL.py
@@ -2,8 +2,6 @@ from flask import Flask, redirect, url_for, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3
 
-app = Flask(__name__)
-
 conn = sqlite3.connect('users.db')
 c = conn.cursor()
 
@@ -14,6 +12,30 @@ conn.commit()
 conn.close()
 
 
+class MyApp(Flask):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+app = MyApp(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+
+def check_user(username, email):
+    user = User.query.filter_by(username=username, email=email).first()
+    if user:
+        return True
+    else:
+        return False
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
@@ -21,17 +43,36 @@ def register():
         password = request.form['password']
         email = request.form['email']
 
-        conn = sqlite3.connect("users.db")
-        c = conn.cursor()
-
-        c.execute("INSERT INTO users (username, password, email) VALUES (?, ?, ?)", (username, password, email))
-
-        conn.commit()
-        conn.close()
+        new_user = User(username=username, password=password, email=email)
+        if check_user(username, email):
+            return render_template('register_incorrect.html')
+        else:
+            db.session.add(new_user)
+            db.session.commit()
 
         return render_template('login.html')
 
     return render_template('register.html')
+
+
+@app.route("/register_incorrect", methods=["GET", "POST"])
+def register_incorrect():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+
+        new_user = User(username=username, password=password, email=email)
+        if check_user(username, email):
+            return render_template('register_incorrect.html')
+        else:
+            db.session.add(new_user)
+            db.session.commit()
+
+        return render_template('login.html')
+
+    return render_template('register.html')
+
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -40,18 +81,12 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
-        conn = sqlite3.connect("users.db")
-        c = conn.cursor()
-
-        c.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
-        user = c.fetchone()
-
-        conn.close()
+        user = User.query.filter_by(username=username, password=password).first()
 
         if user:
-            return render_template("home_page.html")  # после логина переправка на home
+            return render_template("home_page.html")
         else:
-            return render_template("login_incorrect.html")  # если неправильно введены данные, то обратно
+            return render_template("login_incorrect.html")
 
     return render_template('login.html')
 
@@ -76,9 +111,13 @@ def login_incorrect():
         else:
             return render_template("login_incorrect.html")  # если неправильно введены данные, то обратно
 
-    return render_template('login.html')
+    return render_template('login_incorrect.html')
+
+
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
 
 
